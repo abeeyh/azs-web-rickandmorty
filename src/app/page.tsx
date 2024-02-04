f45d6b108643c91054609ae62815eb7f4d7f9c50
Abeeyh <messina@hotmail.com.br>
2024-02-03 22:04:46 -0300

    chore: remove apolo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,10 @@ import { ThemeProvider } from '@mui/material/styles';
 import { darkTheme } from '../provider/theme/dark.theme';
 import Card from '../components/molecules/Card/Card';
 import { Grid } from '@mui/material';
-import { useQuery, gql } from '@apollo/client';
-
-const GET_CHARACTER = gql`
-  query GetCharacter {
-    characters {
-      results {
-        name
-        image
-      }
-    }
-  }
-`;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const ClientSideComponent = () => {
   const [isMounted, setIsMounted] = useState(false);
-  const { loading, error, data } = useQuery(GET_CHARACTER);
 
   useEffect(() => {
     setIsMounted(true);
@@ -30,21 +17,13 @@ const ClientSideComponent = () => {
     return null;
   }
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
   return (
     <ThemeProvider theme={darkTheme}>
       <Grid container style={{ height: '100vh' }}>
         <Card
-          title={data.characters.results[0].name}
+          title={`data.characters.results[0].name`}
           description="desc"
-          imageUrl={data.characters.results[0].image}
+          imageUrl={`data.characters.results[0].image`}
         ></Card>
       </Grid>
     </ThemeProvider>
